fde93479f5a3460dc6d308aba2b499005b23cd10
maycollozano70082 <maycol.lozano70082@ucaldas.edu.co>
2026-06-18 18:27:47 -0500

    grupo y Seleccion

--- src/Model/Grupo.java
+++ src/Model/Grupo.java
@@ -1,4 +1,34 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Grupo {
+
+    private  String nombre;
+    private  ArrayList<Seleccion>seleccions;
+
+    public Grupo(String nombre){
+        this.nombre = nombre;
+        this.seleccions = new ArrayList<>();
+    }
+
+    public void agregarSeleccion(Seleccion seleccion){
+        seleccions.add(seleccion);
+    }
+
+    public void mostrarSeleccions(){
+        System.out.println("Grupo" + nombre);
+
+        for (Seleccion seleccion : seleccions){
+            System.out.println("-" + seleccion.getNombre());
+        }
+    }
+
+    public String getNombre(){
+        return  nombre;
+    }
+
+    public ArrayList<Seleccion> getSeleccions(){
+        return seleccions;
+    }
 }
--- src/Model/Seleccion.java
+++ src/Model/Seleccion.java
@@ -1,4 +1,42 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Seleccion {
+
+    private String nombre;
+  private String confederacion;
+  private DirectorTecnico tecnico;
+  private  ArrayList<Jugador> jugadors;
+
+  public  Seleccion(String nombre,String confederacion, DirectorTecnico tecnico){
+      this.nombre = nombre;
+      this.confederacion = confederacion;
+      this.tecnico = this.tecnico;
+      this.jugadors = new ArrayList<>();
+  }
+public void agregarJugador(Jugador jugador){
+      jugadors.add(jugador);
+}
+public void mostrarJugadores(){
+      for (Jugador jugador : jugadors){
+          System.out.println(
+                  jugador.getNombre() +
+                          "- Dorsal: " + jugador.getDorsal() +
+                          "- posición " + jugador.getPosicion()
+          );
+      }
+}
+public  String getNombre(){
+      return nombre;
+}
+public  String getConfederacion (){
+      return confederacion;
+}
+public DirectorTecnico getTecnico(){
+      return tecnico;
+}
+public ArrayList<Jugador> getJugadors(){
+      return jugadors;
+}
 }
